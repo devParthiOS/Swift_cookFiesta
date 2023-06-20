03c1260b07d7c9032b548927d167f3301aecec47
Shreyank Vyas <shreyank.vyas@alliancetek.com>
2023-06-20 11:13:45 +0530

    Second Commit

--- CookFiesta/AppDelegate.swift
+++ CookFiesta/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate  {
     }
 
     // MARK: UISceneSession Lifecycle
+    // Done
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
